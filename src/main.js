fcad7085bd71ca725d7a2e9a4696c325bc3e33c3
xingyys <598223084@qq.com>
2018-03-07 10:24:30 +0800

    修改bug

--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import G2 from 'vue-g2'
+// import Vue from 'vue'
+// import App from './App'
+// import G2 from 'vue-g2'
 
-Vue.config.productionTip = false
-/* eslint-disable no-new */
-Vue.use(G2)
+// Vue.config.productionTip = false
+// /* eslint-disable no-new */
+// Vue.use(G2)
 
-new Vue({
-    el: '#app',
-    components: { App },
-    template: '<App/>'
-})
+// new Vue({
+//     el: '#app',
+//     components: { App },
+//     template: '<App/>'
+// })
